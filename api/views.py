17eb473d45a2ff197cae82c59ffae3b0dd60c5e5
djumanov <diyorbekjumanov1120@gmail.com>
2022-10-15 16:29:00 +0500

    add prod

--- api/views.py
+++ api/views.py
@@ -3,22 +3,22 @@ from django.http import HttpRequest, JsonResponse, HttpResponse
 from .models import Product
 
 # Create your views here.
-def get_products(request: HttpRequest) -> list[dict]:
+def get_products(request: HttpRequest) -> JsonResponse:
     if request.method == 'GET':
         products  = Product.objects.all()
         all_products = list()
         for product in products:
             all_products.append({
-                'id': product.id,
-                'name': product.name,
-                'company': product.company,
-                'color': product.color,
-                'RAM': product.RAM,
-                'memory': product.memory,
-                'price': product.price,
+                'id':         product.id,
+                'name':       product.name,
+                'company':    product.company,
+                'color':      product.color,
+                'RAM':        product.RAM,
+                'memory':     product.memory,
+                'price':      product.price,
                 'created_at': product.created_at,
                 'updated_at': product.updated_at,
-                'img_url': product.img_url,
+                'img_url':    product.img_url,
             })
 
         return JsonResponse({'products': all_products})
@@ -26,21 +26,50 @@ def get_products(request: HttpRequest) -> list[dict]:
     return HttpResponse('Bad Request!')
 
 
-def get_product(request: HttpRequest, id: int) -> dict:
+def get_product(request: HttpRequest, id: int) -> JsonResponse:
     if request.method == 'GET':
         product = Product.objects.get(id=id)
         #Check if the product exists using the id    
         product_json = {
-                'id': product.id,
-                'name': product.name,
-                'company': product.company,
-                'color': product.color,
-                'RAM': product.RAM,
-                'memory': product.memory,
-                'price': product.price,
+                'id':         product.id,
+                'name':       product.name,
+                'company':    product.company,
+                'color':      product.color,
+                'RAM':        product.RAM,
+                'memory':     product.memory,
+                'price':      product.price,
                 'created_at': product.created_at,
                 'updated_at': product.updated_at,
-                'img_url': product.img_url,
+                'img_url':    product.img_url,
             }
 
         return JsonResponse({'product': product_json})
+
+
+def add_product(request: HttpRequest) -> JsonResponse:
+    if request.method == 'POST':
+        print('-------okokokokokokok-------')
+        data = request.POST
+        new_product = Product()
+        new_product.name    = data['name']
+        new_product.company = data['company']
+        new_product.color   = data['color']
+        new_product.RAM     = data['RAM']
+        new_product.memory  = data['memory']
+        new_product.price   = data['price']
+        new_product.img_url = data['img_url']
+        new_product.save()
+
+        product_json = {
+                'id':         new_product.id,
+                'name':       new_product.name,
+                'company':    new_product.company,
+                'color':      new_product.color,
+                'RAM':        new_product.RAM,
+                'memory':     new_product.memory,
+                'price':      new_product.price,
+                'created_at': new_product.created_at,
+                'updated_at': new_product.updated_at,
+                'img_url':    new_product.img_url,
+            }
+        return JsonResponse({'new_product': product_json})
--- config/urls.py
+++ config/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from api.views import get_products, get_product
+from api.views import get_products, get_product, add_product
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/products/', get_products),
     path('api/product/<int:id>', get_product),
+    path('api/add_product/', add_product),
 ]
